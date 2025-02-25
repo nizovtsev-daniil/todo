351e05e1de380fb4f3b10d2d3cd9436624c142b3
nizovtsev-daniil <nizovtsev.work@gmail.com>
2025-02-25 21:53:46 +0300

    add github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [svgr(), react()],
+  base: '/todo',
+  plugins: [svgr(), react()],
 });
